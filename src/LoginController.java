e104dc49708ae15da33956db3659fc7b9a05fce8
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-21 16:33:13 +0700

    reformat code

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,29 +1,34 @@
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginController", urlPatterns = "/login")
 public class LoginController extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
 
         String username = request.getParameter("txtUsername");
         String password = request.getParameter("txtPassword");
-        
+
+
         HttpSession session = request.getSession();
         if (username.equals("admin") && password.equals("admin")) {
 
             session.setAttribute("username", "admin");
 
-            RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
-            dis.forward(request, response);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+            dispatcher.forward(request, response);
         } else {
 
-            request.setAttribute("error", "Username and Password invalid !");
+            request.setAttribute("error", "Invalid username or password");
 
-            RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
-            dis.forward(request, response);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+
         }
     }
 }
