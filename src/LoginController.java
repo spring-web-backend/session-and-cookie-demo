1ef8c256dac40725541df3ab3b781313a035b940
Minh Lee <minhlee.fre.mta@gmail.com>
2018-09-21 16:19:14 +0700

    create login form

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,37 +1,29 @@
-import sun.security.x509.RDN;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet(name = "LoginController", urlPatterns = "/login")
 public class LoginController extends HttpServlet {
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String username = request.getParameter("txtUsername");
         String password = request.getParameter("txtPassword");
-
+        
         HttpSession session = request.getSession();
         if (username.equals("admin") && password.equals("admin")) {
-            session.setAttribute("admin", "admin");
 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
-            dispatcher.forward(request, response);
+            session.setAttribute("username", "admin");
+
+            RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
+            dis.forward(request, response);
         } else {
-            request.setAttribute("error", "Invalid username or password");
 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-            dispatcher.forward(request, response);
+            request.setAttribute("error", "Username and Password invalid !");
+
+            RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+            dis.forward(request, response);
         }
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
-
     }
 }
